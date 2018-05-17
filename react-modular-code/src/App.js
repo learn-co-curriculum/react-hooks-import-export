7f137ec170d3850b4cee2cf83cf54ada4f1029f7
Matt Lawford <Matt@nyc-055668-mbp.local>
2018-05-17 15:47:32 -0400

    updated readme

---
+++ react-modular-code/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hogwarts from './hogwarts'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hogwarts/>
+      </div>
+    );
+  }
+}
+
+export default App;
